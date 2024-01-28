42bb38c545d58676998bf8afab6f8efc270cfc87
Anatol Hancharou <anatolhancharou@gmail.com>
2024-01-28 22:46:53 +0300

    feat: add streams/transform.js task implementation

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { EOL } from 'node:os';
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+        transform(chunk, _, callback) {
+            const output = chunk.toString().trim().split('').reverse().join('');
+            callback(null, `${output}${EOL}`);
+        },
+    });
+
+    await pipeline(process.stdin, reverse, process.stdout);
 };
 
 await transform();
