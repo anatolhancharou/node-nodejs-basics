ca8b20e5bb9eb24fdd6d2c12bf6d7b650331db8e
Anatol Hancharou <anatolhancharou@gmail.com>
2024-01-28 22:07:53 +0300

    feat: add fs/delete.js task implementation

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { rm } from 'node:fs/promises';
+import { getPath } from '../services.js';
+import { errorMessage } from './constants.js';
+
+const fileToRemovePath = getPath(import.meta.url, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await rm(fileToRemovePath);
+    } catch {
+        throw new Error(errorMessage);
+    }
 };
 
 await remove();
