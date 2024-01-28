18f94f5ca721c6359802a932871809418965b6f9
Anatol Hancharou <anatolhancharou@gmail.com>
2024-01-28 22:06:25 +0300

    feat: add fs/rename.js task implementation

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import { rename as fsRename } from 'node:fs/promises';
+import { getPath } from '../services.js';
+import { errorMessage } from './constants.js';
+
+const currentFilePath = getPath(import.meta.url, 'files', 'wrongFilename.txt');
+const renamedFilePath = getPath(import.meta.url, 'files', 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        await fsRename(currentFilePath, renamedFilePath);
+    } catch {
+        throw new Error(errorMessage);
+    }
 };
 
 await rename();
