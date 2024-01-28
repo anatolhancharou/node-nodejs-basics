a7db4aa4102fc2a26c3bca3cfaa61603d5fbfb07
Anatol Hancharou <anatolhancharou@gmail.com>
2024-01-28 22:50:05 +0300

    feat: add zip/decompress.js task implementation

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { getPath } from '../services.js';
+
+const compressedFilePath = getPath(import.meta.url, 'files', 'archive.gz');
+const decompressedFilePath = getPath(
+    import.meta.url,
+    'files',
+    'fileToCompress.txt'
+);
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(compressedFilePath);
+    const writeStream = createWriteStream(decompressedFilePath);
+    await pipeline(readStream, createGunzip(), writeStream);
 };
 
 await decompress();
